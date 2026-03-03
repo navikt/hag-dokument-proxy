8ad031673376add73de88e04674d5b38e903d36d
Kent Fjellstad <kent.fjellstad@nav.no>
2026-03-03 10:30:56 +0100

    Legger til dekoratøren på feilmelding siden

--- src/server.js
+++ src/server.js
@@ -1,23 +1,50 @@
 import express from "express";
 import { getToken, validateToken } from "@navikt/oasis";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
+import {
+  buildCspHeader,
+  injectDecoratorServerSide,
+} from "@navikt/nav-dekoratoren-moduler/ssr/index.js";
+
 const app = express();
 
 // Redirect må komme FØR static middleware
 app.get("/", async (req, res) => {
   const token = getToken(req);
   if (!token) {
-    res.redirect(`/feilmelding`);
+    return res.redirect(`/feilmelding`);
   }
   const validation = await validateToken(token);
   if (!validation.ok) {
-    res.redirect(`/feilmelding`);
+    return res.redirect(`/feilmelding`);
   }
   res.redirect(`/success`);
 });
 
 // Mount static files på /feilmelding path
-app.use("/feilmelding", express.static("dist/feilmelding"));
+app.use("/feilmelding", function (req, res) {
+  injectDecoratorServerSide({
+    env: "prod",
+    filePath: "dist/feilmelding/index.html",
+    params: { context: "arbeidsgiver" },
+  })
+    .then((html) => {
+      const csp = buildCspHeader(
+        {},
+        {
+          env: "prod",
+          params: { context: "arbeidsgiver" },
+        },
+      );
+      res.setHeader("Content-Security-Policy", csp);
+      res.send(html);
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-undef
+      console.error("Server: SSR error", error);
+      res.status(500).send("500 Error");
+    });
+});
 
 app.use("/success", express.static("dist/success"));
 
